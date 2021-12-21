2f8c2908af1a86253288bedd0e7b044ff3d4125a
sanjeth suthahar <sanjeth00@live.no>
2021-12-21 01:34:45 +0100

    'lel'

--- src/com/company/GameWindow.java
+++ src/com/company/GameWindow.java
@@ -1,4 +1,36 @@
 package com.company;
 
-public class GameWindow {
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    private JPanel layout, chessBoard, menu;
+    private JButton[][] squares = new JButton[8][8];
+
+    public GameWindow() {
+
+        JFrame gameWindow = new JFrame("Chess");
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setSize(1920,1080);
+        gameWindow.setLayout(null);
+        gameWindow.getContentPane().setBackground(Color.BLACK);
+
+        layout = new JPanel();
+        chessBoard = new JPanel();
+        menu = new JPanel();
+
+        layout.setBounds(60,30,800,800);
+        chessBoard.setBounds(140,110,640,640);
+        layout.setBackground(new Color(150,75,0));
+        menu.setBounds(900,30,400,800);
+        chessBoard.setLayout(new GridLayout(0,9));
+        chessBoard.setBorder(new LineBorder(Color.black));
+
+        gameWindow.add(chessBoard);
+        gameWindow.add(layout);
+        gameWindow.add(menu);
+        gameWindow.setVisible(true);
+    }
 }
